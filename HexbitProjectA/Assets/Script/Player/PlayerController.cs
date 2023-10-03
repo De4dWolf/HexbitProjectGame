465d5b48a1ceaef59b63c6cd09ce5c21553b0b6a
De4dWolf <crypt.wolfi3@gmail.com>
2023-10-03 19:13:02 +0700

    initial commit

---
+++ HexbitProjectA/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    private float groundCheckRadius = 0.2f;
+    private Rigidbody2D rb;
+    public GameObject CharacterControl2;
+
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        movement();
+    }
+
+
+    private void movement()
+    {
+        // Deteksi apakah karakter berada di atas tanah.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Mengontrol pergerakan karakter.
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+        //Debug.DrawRay(groundCheck.position, Vector2.down * groundCheckRadius, Color.red);
+
+        // Melompat jika pemain menekan tombol lompat dan karakter berada di atas tanah.
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            CharacterControl2.SetActive(false);
+        }
+    }
+}
