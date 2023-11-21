391c35a7a7deff1decc7e468898051839a6a2eca
De4dWolf <crypt.wolfi3@gmail.com>
2023-11-21 19:06:04 +0700

    Update Selasa 08:03
    Level 1 Added (70% Complate)

--- HexbitProjectA/Assets/Script/ObjectScript/JumpPad.cs
+++ HexbitProjectA/Assets/Script/ObjectScript/JumpPad.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    private float bounce = 20f;
+    [SerializeField] private float bounce = 70f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
